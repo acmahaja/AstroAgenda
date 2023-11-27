7480b43e28e2a530fe37d4d6f53d55ad427bca95
@acmahaja <acmahaja@gmail.com>
2023-11-27 14:32:15 +0200

    Added basic landing route

--- backend/src/test/java/app/astroagenda/backend/BackendApplicationTests.java
+++ backend/src/test/java/app/astroagenda/backend/BackendApplicationTests.java
@@ -1,10 +1,10 @@
-package app.astroagenda.astroagenda;
+package app.astroagenda.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AstroagendaApplicationTests {
+class BackendApplicationTests {
 
 	@Test
 	void contextLoads() {

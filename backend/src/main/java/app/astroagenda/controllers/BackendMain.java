1807354e6a012e9e8a411d314e9128b23d5cac87
@acmahaja <acmahaja@gmail.com>
2023-11-27 18:29:35 +0200

    Added controllers, entities, mappers, service and utils

--- backend/src/main/java/app/astroagenda/controllers/BackendMain.java
+++ backend/src/main/java/app/astroagenda/controllers/BackendMain.java
@@ -1,16 +1,18 @@
-package app.astroagenda.backend;
+package app.astroagenda.controllers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 @RestController
 public class BackendMain {
 
 	public static void main(String[] args) {
+		Dotenv.configure().load();
 		SpringApplication.run(BackendMain.class, args);
 	}
 

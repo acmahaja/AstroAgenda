d7a7171ad6976df62b36c5280074c0ee1489be00
@acmahaja <acmahaja@gmail.com>
2023-12-08 13:55:24 +0200

    Added twinkling star background

---
+++ frontend/src/components/NavBar.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Navbar() {
+    return (<nav className="flex align-middle justify-between">
+        <img src="/logo_transparent.svg" alt="logo" />
+    </nav>);
+}
+
+export default Navbar;
---
+++ frontend/src/pages/UnderDevelopment.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Navbar from "../components/NavBar"
+
+function UnderDevelopment() {
+    return (
+    <div className="background-container">
+        <div className="stars" />
+        <div className="twinkling" />
+      </div>
+  
+    )
+    
+
+
+}
+
+
+export default UnderDevelopment;
